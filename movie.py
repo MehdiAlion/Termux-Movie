901edc1e6291fc87c1bd5cb52752b264ddde7d9e
Mehdi4lion <74841880+Mehdi4lion@users.noreply.github.com>
2021-01-23 02:18:18 +0330

    Add files via upload

---
+++ movie.py
@@ -0,0 +1,27 @@
+import os
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+os.system("clear")
+print(f"{pink} ")
+os.system("pkg install figlet -y ")
+os.system("clear")
+os.system("figlet PersianElite")
+print(f"{rang} ")
+print("∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆")
+print(f"{blue} ")
+print("Telegram : @Persian_Elite")
+print("YouTube  : YouTube.com/PersianElite")
+print(f"{rang} ")
+print("∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆")
+print(f"{green} ")
+print("{1}Start")
+print("{2}Exit")
+Elite = int(input("|∆|choose <>> :"))
+if Elite == 1:
+    os.system("telnet towel.blinkenlights.nl")
+elif Elite == 2:
+    os.system("clear")
+    os.system("figlet PersianElite")
